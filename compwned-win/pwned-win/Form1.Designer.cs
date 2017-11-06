9cdfa88c9db1a4fb74e17addff2ddea6b6c05d1b
Salessawi Ferede Yitbarek <salessaf@umich.edu>
2017-11-06 18:33:48 -0500

    windows app added

---
+++ compwned-win/pwned-win/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace pwned_win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textPass
+            // 
+            this.textPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPass.Location = new System.Drawing.Point(15, 38);
+            this.textPass.Name = "textPass";
+            this.textPass.PasswordChar = '*';
+            this.textPass.Size = new System.Drawing.Size(505, 32);
+            this.textPass.TabIndex = 1;
+            this.textPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textPass.TextChanged += new System.EventHandler(this.textPass_TextChanged);
+            this.textPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textPass_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(534, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.BackColor = System.Drawing.Color.RoyalBlue;
+            this.labelResult.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.ForeColor = System.Drawing.Color.White;
+            this.labelResult.Location = new System.Drawing.Point(0, 99);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(641, 109);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel.LinkColor = System.Drawing.Color.Azure;
+            this.linkLabel.Location = new System.Drawing.Point(236, 156);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(169, 18);
+            this.linkLabel.TabIndex = 5;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "haveibeenpwned.com";
+            this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Password:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(641, 208);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textPass);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ComPWNED Desktop (Beta)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textPass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ compwned-win/pwned-win/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Numerics;
+using System.IO.MemoryMappedFiles;
+using System.Collections;
+
+
+
+namespace pwned_win
+{
+    public partial class Form1 : Form
+    {
+
+        const int NUM_HASHES = 10;
+        const long BIT_ARRAY_SIZE = 4615205609;
+        const string FILE_NAME = "hibp_4615205609_10.bin";
+
+        private MemoryMappedFile mem_mapped = null;
+        MemoryMappedViewAccessor accessor = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            search();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LinkLabel.Link link = new LinkLabel.Link();
+            link.LinkData = "https://haveibeenpwned.com/Passwords";
+
+            linkLabel.Links.Add(link);
+
+
+            this.AcceptButton = this.button1;
+        }
+
+        private void textPass_TextChanged(object sender, EventArgs e)
+        {
+            labelResult.Text = "";
+            labelResult.BackColor = this.BackColor;
+            linkLabel.Visible = true;
+        }
+
+
+        private byte[] convert_to_unsigned(byte[] hash_bytes)
+        {
+            if (hash_bytes[hash_bytes.Length - 1] != 0)
+            {
+                byte[] temp = new byte[hash_bytes.Length + 1];
+                Array.Copy(hash_bytes, temp, hash_bytes.Length);
+                temp[temp.Length - 1] = 0;
+                return temp;
+            }
+
+            else
+            {
+                return hash_bytes;
+            }
+        }
+
+        private string get_sha1_string(string str_value)
+        {
+            byte[] byte_arr = Encoding.Default.GetBytes(str_value);
+
+            SHA1 sha = new SHA1CryptoServiceProvider();
+
+            byte[] hash_val = sha.ComputeHash(byte_arr);
+
+
+            string hash_str = BitConverter.ToString(hash_val).Replace("-", "");
+
+            return hash_str.ToUpper();
+            
+
+        }
+
+
+        private long get_bit_index(string str_value) {
+
+            BigInteger table_size = new BigInteger(BIT_ARRAY_SIZE);
+            BigInteger index;
+
+            byte[] byte_arr = Encoding.Default.GetBytes(str_value);
+
+            SHA1 sha = new SHA1CryptoServiceProvider();
+
+           byte[] hash_val = sha.ComputeHash(byte_arr);
+           Array.Reverse( hash_val );
+
+            hash_val = convert_to_unsigned(hash_val);
+            
+
+            //System.Console.WriteLine(BitConverter.ToString(hash_val).Replace("-", ""));
+
+            BigInteger int_hash = new BigInteger(hash_val);
+            //System.Console.WriteLine("Int Hash:" + int_hash.ToString());
+
+
+            BigInteger.DivRem(int_hash, table_size, out index);
+
+            //System.Console.WriteLine("Index:" + ((long)index).ToString());
+            return (long)index;
+
+        }
+
+        private bool is_bit_set(MemoryMappedViewAccessor accessor, long offset)
+        {
+            byte val =  accessor.ReadByte(offset/8);
+
+            //System.Console.WriteLine("Byte Val:\t" + val.ToString());
+
+            UInt16 bit_index = (UInt16)(7 - offset % 8);
+
+            bool bit = ((val >> bit_index) & 1) == 1;
+
+            //System.Console.WriteLine("Bit Val:\t" + bit.ToString());
+
+
+            return bit;
+
+        }
+        
+
+
+        private void search()
+        {
+
+            //get the password as a byte array
+            string pass = textPass.Text;
+
+            if (pass == "") return;
+
+
+            //map bloom filter ...
+            if (mem_mapped == null)
+            {
+                mem_mapped =
+                MemoryMappedFile.CreateFromFile(FILE_NAME, System.IO.FileMode.Open, "BLOOM");
+                accessor = mem_mapped.CreateViewAccessor();
+            }
+
+            
+            
+
+            bool found = true;
+
+            long index = get_bit_index(pass);
+
+
+            found = found & is_bit_set(accessor, index);
+
+            string password_sha1 = get_sha1_string(pass);
+            //System.Console.WriteLine(password_sha1);
+            for (int i = 0; i< NUM_HASHES-1; i++)
+            {
+
+                index = get_bit_index(password_sha1 + i.ToString());
+                found = found & is_bit_set(accessor, index);
+
+            }
+
+            linkLabel.Visible = false;
+
+            if (found)
+            {
+                labelResult.Text = "Oh No! \n It is highly likely this password has previously appeared in a data breach. Change it immediately! !";
+                labelResult.BackColor = Color.Maroon;
+            }
+            else
+            {
+                labelResult.Text = "Good news — not found in list of breached passwords!";
+                labelResult.BackColor = Color.Green;
+            }
+
+            
+
+            /*System.Console.WriteLine(BitConverter.ToString(hash_val).Replace("-",""));
+            System.Console.WriteLine(table_size.ToString());
+            System.Console.WriteLine(int_val.ToString());
+            System.Console.WriteLine(index.ToString());*/
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Link.LinkData as string);
+        }
+
+        private void textPass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+    }
+}
